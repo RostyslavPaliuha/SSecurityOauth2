a0bfd34ef15276a485445ca58fc18a052e1b3a1f
rostyslavpaliuha <rostyslavpaliuha@gmail.com>
2018-02-21 22:16:52 +0200

    worked security based on spring modules and oauth2

---
+++ src/main/java/media/redStone/authRestService/testTask/configuration/MainJavaConfig.java
@@ -0,0 +1,13 @@
+package media.redStone.authRestService.testTask.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "media.redStone.authRestService.testTask")
+public class MainJavaConfig {
+	
+
+}
---
+++ src/main/java/media/redStone/authRestService/testTask/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package media.redStone.authRestService.testTask.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
